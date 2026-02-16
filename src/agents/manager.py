1e7ec4c83d0851532d1347fd9b07d3021d1e0f62
Alexander Bobrovsky <bobrovsky@seznam.cz>
2026-02-16 22:25:28 +0100

    Refactoring of task creation

--- src/agents/manager.py
+++ src/agents/manager.py
@@ -2,21 +2,24 @@ import importlib
 import yaml
 from crewai import Agent, Task
 
+project_schema = importlib.import_module('project.schema')
+
 def create_tasks(agents: dict[str, Agent]) -> list[Task]:
     """
     Create a list of tasks for the crew based on the project description
     """
     file_name = 'agents/workflow.yml'
-    project_schema = importlib.import_module('project.schema')
     with open(file_name, 'r') as f:
         data = yaml.safe_load(f)
-    return [
-        Task(
-            name=task['name'],
-            agent=agents[task['agent']],
-            description=task['description'],
-            expected_output=task['expected output'],
-            output_pydantic=getattr(project_schema, task['output pydantic']) if 'output pydantic' in task else None
-        )
-        for task in data.values()
-    ]
+    return [new_task(task, agents) for task in data.values()]
+
+def new_task(data: dict, agents: dict[str, Agent]) -> Task:
+    task_config = {
+        'name': data['name'],
+        'agent': agents[data['agent']],
+        'description': data['description'],
+        'expected_output': data['expected output'],
+    }
+    if 'output pydantic' in data:
+        task_config['output_pydantic'] = getattr(project_schema, data['output pydantic'])
+    return Task(**task_config)
