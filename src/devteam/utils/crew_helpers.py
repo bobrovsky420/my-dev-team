c0011f6a20793126b909903b4038d032d7fe52c8
Alexander Bobrovsky <bobrovsky@seznam.cz>
2026-03-17 17:03:22 +0100

    Small refactoring

--- src/devteam/utils/crew_helpers.py
+++ src/devteam/utils/crew_helpers.py
@@ -4,13 +4,16 @@ from ..settings import get_config_dir
 
 logger = logging.getLogger("Crew Factory")
 
+def load_crew_config(config_name: str) -> dict:
+    config_path = get_config_dir() / 'crews' / config_name
+    return yaml.safe_load(config_path.read_text(encoding='utf-8'))
+
 def build_agents_from_config(config_name: str) -> dict:
     """Agents factory"""
     # pylint: disable=import-outside-toplevel,cyclic-import
     import devteam.agents as agents_module
     import devteam.tools as tools_module
-    config_path = get_config_dir() / 'crews' / config_name
-    crew_config = yaml.safe_load(config_path.read_text(encoding='utf-8'))
+    crew_config = load_crew_config(config_name)
     agents = {}
     for node_name, details in crew_config.get('agents', {}).items():
         class_name = details['class']
@@ -23,7 +26,7 @@ def build_agents_from_config(config_name: str) -> dict:
         if sandbox_class := details.get('sandbox', None):
             ToolsClass = getattr(tools_module, sandbox_class, None)
             if not ToolsClass:
-                raise ValueError(f"Configuration Error: '{class_name}' is not a valid class in devteam.tools")
+                raise ValueError(f"Configuration Error: '{sandbox_class}' is not a valid class in devteam.tools")
             logger.debug("Adding %s tool to '%s'...", sandbox_class, node_name)
             agents[node_name] = agents[node_name].with_sandbox(ToolsClass())
     return agents
